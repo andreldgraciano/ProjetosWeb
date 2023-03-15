d26a1d069d39a04908602ea5044c5b6844ab82c3
Dedasgdias <dedasgdias@gmail.com>
2023-03-14 21:45:44 -0300

    projeto 9

---
+++ 9-projeto-Mata-mosquito/README.md
@@ -0,0 +1,28 @@
+# Projeto 9 - Mata mosquito
+
+Este projeto se trata de uma réplica de um jogo chamado Mata mosquito. Com esse projeto, eu pude testar os conhecimentos e aplicar os elementos como:
+
+##Java Script
+- Variáveis
+- Concatenação
+- Operadores Lógicos, condicionais
+- Desvios condicionais
+- Casting
+- Operador ternário
+- Precedência
+- Funções
+- Escopo
+- Funcões anônimas e técnica de wrapper
+- Funcões de callback
+- Eventos
+- DOM
+- Arrays
+- Estruturas de repetição
+- Parâmetros variáveis
+- Try, Catch, Throw e Finally
+- BOM
+
+<img src="/index.png" width="400px">
+<img src="/app.png" width="400px">
+<img src="/vitoria.png" width="400px">
+<img src="/fim_de_jogo.png" width="400px">
---
+++ 9-projeto-Mata-mosquito/js/script.js
@@ -0,0 +1,91 @@
+var largura = 0
+var altura = 0
+var vidas = 1
+var tempo = 15
+
+var nivel = window.location.search.replace('?', '')
+
+if (nivel === 'facil') {
+    criaMosquitoTempo = 1500
+} else if (nivel === 'medio') {
+    criaMosquitoTempo = 1000
+} else if (nivel === 'dificil') {
+    criaMosquitoTempo = 750
+}
+
+function ajustarTamanhoJogo() {
+    largura = window.innerWidth
+    altura = window.innerHeight
+}
+
+ajustarTamanhoJogo()
+
+var cronometro = setInterval(function () {
+    tempo -= 1
+
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(cronometro)
+        window.location.href = 'vitoria.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+
+}, 1000)
+
+function posicaoRandomica() {
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if (vidas > 3) {
+            return window.location.href = 'fim_de_jogo.html'
+        } else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
+    }
+
+    var posicaoX = Math.floor(Math.random() * largura)
+    var posicaoY = Math.floor(Math.random() * altura)
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+
+    var tamanho = tamanhoAleatorio()
+    mosquito.className = tamanho[0] + ' ' + ladoAleatorio()
+
+    posicaoX = posicaoX >= largura - tamanho[1] ? largura - tamanho[1] : posicaoX
+    posicaoY = posicaoY >= altura - tamanho[1] ? altura - tamanho[1] : posicaoY
+
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function () {
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    switch (classe) {
+        case 0:
+            return ['mosquito1', 50]
+        case 1:
+            return ['mosquito2', 70]
+        case 2:
+            return ['mosquito3', 90]
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
